1ca2e9c9a37d4afd350e6282261b631d36f95364
Ho Nguyen <48714123+honguyen-wize@users.noreply.github.com>
2025-11-19 15:12:01 +0700

    Generate automation for QAAGENT-57

---
+++ tests/feature_homepage/productList.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+import { POManager } from '../../page-objects/POManager';
+
+test.describe('Product List Display Validation', () => {
+  let poManager: POManager;
+
+  test.beforeEach(async ({ page }) => {
+    poManager = new POManager(page);
+  });
+
+  test('@QAAGENT-57 Verify that products are properly displayed on the home page', async ({ page }) => {
+    await poManager.getHomepage().goToHomepage();
+    await poManager.getHomepage().verifyProductList();
+    await poManager.getHomepage().verifyProductIsDisplayed('Samsung galaxy s6');
+  });
+});
