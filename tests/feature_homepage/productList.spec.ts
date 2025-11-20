2b8eaf55b428df852754781cbb6f234bf83e2823
Trang Than <108047986+trang-than@users.noreply.github.com>
2025-11-20 16:03:58 +0700

    Generate automation for QAAGENT-44

---
+++ tests/feature_homepage/productList.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+import { POManager } from '../../page-objects/POManager';
+
+test.describe('Product List Display Verification', () => {
+  let poManager: POManager;
+
+  test.beforeEach(async ({ page }) => {
+    poManager = new POManager(page);
+  });
+
+  test('@QAAGENT-44 Verify that products are properly displayed on the home page', async ({ page }) => {
+    await poManager.getHomepage().goToHomePage();
+    await poManager.getHomepage().verifyProductList();
+    await poManager.getHomepage().verifyProductIsDisplayed('Samsung galaxy s6');
+  });
+});
