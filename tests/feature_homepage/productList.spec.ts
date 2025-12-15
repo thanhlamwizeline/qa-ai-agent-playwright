921e6892f7a9935004182568e6119dc745d3e6e9
Ho Nguyen <48714123+honguyen-wize@users.noreply.github.com>
2025-12-15 10:53:53 +0700

    Generate automation for QAAGENT-44

---
+++ tests/feature_homepage/productList.spec.ts
@@ -0,0 +1,32 @@
+import { test } from '@playwright/test';
+
+import dotenv from 'dotenv';
+
+import { TESTCONFIG } from '../../data/config/testconfig';
+import { POManager } from '../../page-objects/POManager';
+
+dotenv.config();
+
+test.describe('@feature_homepage Product List Verification', () => {
+  let poManager: POManager;
+
+  test.beforeEach(async ({ page }) => {
+    poManager = new POManager(page);
+  });
+
+  test('@QAAGENT-44 Verify that products are properly displayed on the home page', async () => {
+    const homePage = poManager.getHomepage();
+
+    await test.step('Navigate to the home page (/)', async () => {
+      await homePage.navigateToHomePage();
+    });
+
+    await test.step('Verify the product container element is visible (products table body)', async () => {
+      await homePage.verifyProductList();
+    });
+
+    await test.step('Verify "Samsung galaxy s6" product is displayed in the product list', async () => {
+      await homePage.verifyProductIsDisplayed('Samsung galaxy s6');
+    });
+  });
+});
